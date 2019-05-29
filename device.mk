dff76772975a76b1c2fb22cb8368208272273d2b
Flagers <flagersmail@gmail.com>
2019-05-29 14:45:20 -0400

    owens: remove vibrator hal in order to bring cedric vibrator hal

--- device.mk
+++ device.mk
@@ -308,7 +308,8 @@ PRODUCT_PACKAGES += \
 
 # Vibrator HAL
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-service.addison
+	android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service.owens
 
 # Wifi
 PRODUCT_PACKAGES += \
