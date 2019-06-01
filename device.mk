b779b6624f14524f79f50764953c6aa758d674a1
Flagers <flagersmail@gmail.com>
2019-06-01 02:17:28 -0400

    owens: add sensor related things and update p-files.txt

--- device.mk
+++ device.mk
@@ -277,6 +277,10 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
+    libsensorhub \
+    motosh \
+    sensorhub.msm8937 \
+    sensors.msm8937
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
